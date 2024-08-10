8eeb1d147f8627dfea1ff42121105176f594e634
Mateus Back <m.back@nextage.com.br>
2024-08-10 09:12:34 -0300

    first commit

---
+++ src/main/java/org/therapazes/luisaoproject/LuisaoprojectApplication.java
@@ -0,0 +1,13 @@
+package org.therapazes.luisaoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuisaoprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuisaoprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/therapazes/luisaoproject/LuisaoprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.therapazes.luisaoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuisaoprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
