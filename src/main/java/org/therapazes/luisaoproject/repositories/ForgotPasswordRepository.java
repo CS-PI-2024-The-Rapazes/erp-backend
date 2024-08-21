f4056693ee94c9fdf6e21ceee1729833fdcaf5c8
Rubens Matheus <y2afepolfxx@gmail.com>
2024-08-21 18:42:19 -0300

    feat: enviar email para recuperar senha

--- src/main/java/org/therapazes/luisaoproject/repositories/ForgotPasswordRepository.java
+++ src/main/java/org/therapazes/luisaoproject/repositories/ForgotPasswordRepository.java
@@ -1,8 +1,14 @@
 package org.therapazes.luisaoproject.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.therapazes.luisaoproject.entities.ForgotPassword;
+import org.therapazes.luisaoproject.entities.User;
+
+import java.util.Optional;
 
 public interface ForgotPasswordRepository extends JpaRepository<ForgotPassword, Integer> {
 
+    @Query("SELECT fp FROM ForgotPassword fp WHERE fp.otp = ?1 AND fp.user = ?2")
+    Optional<ForgotPassword> findByOtpAndUser(Integer otp, User user);
 }
--- src/main/java/org/therapazes/luisaoproject/repositories/UserRepository.java
+++ src/main/java/org/therapazes/luisaoproject/repositories/UserRepository.java
@@ -1,13 +1,20 @@
 package org.therapazes.luisaoproject.repositories;
 
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import org.therapazes.luisaoproject.entities.User;
 
 import java.util.Optional;
 
+
+@Repository
 public interface UserRepository {
     Optional<User> findByEmail(String username);
 
-    @Query("update User u")
-    void updatePassword(String email, String password);
+    @Transactional
+    @Modifying
+    @Query("UPDATE User u SET u.password = ?2 WHERE u.email = ?1")
+    void updatePassoword(String email, String password);
 }
