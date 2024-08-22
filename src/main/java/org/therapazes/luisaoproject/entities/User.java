ca7f4ef705e102f769add9adc5f27177b1a93350
Rubens Matheus <y2afepolfxx@gmail.com>
2024-08-21 21:39:58 -0300

    fix: alocando o código para as pastas corretas e correções de dependencias

--- src/main/java/org/therapazes/luisaoproject/entities/User.java
+++ src/main/java/org/therapazes/luisaoproject/entities/User.java
@@ -1,13 +1,22 @@
 package org.therapazes.luisaoproject.entities;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer userId;
+    @Column(name = "user_id")
+    private Integer idUser;
+    @Column(name = "password")
+    private String password;
+    private String email;
+
 
     @OneToOne(mappedBy = "user")
     private ForgotPassword forgotPassword;
--- src/main/java/org/therapazes/luisaoproject/repositories/UserRepository.java
+++ src/main/java/org/therapazes/luisaoproject/repositories/UserRepository.java
@@ -1,6 +1,7 @@
 package org.therapazes.luisaoproject.repositories;
 
 import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -10,8 +11,10 @@ import java.util.Optional;
 
 
 @Repository
-public interface UserRepository {
-    Optional<User> findByEmail(String username);
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    @Query("SELECT u FROM User u WHERE u.email = :email")
+    Optional<User> findByEmail(String email);
 
     @Transactional
     @Modifying
