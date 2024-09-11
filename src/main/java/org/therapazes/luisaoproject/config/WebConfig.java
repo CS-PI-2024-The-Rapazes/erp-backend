b6581e23fd5f37f61e6f9961bbfbcaeabc4518cb
Mateus Back <m.back@nextage.com.br>
2024-09-11 09:58:17 -0300

    fix: troca encoding

--- src/main/java/org/therapazes/luisaoproject/config/WebConfig.java
+++ src/main/java/org/therapazes/luisaoproject/config/WebConfig.java
@@ -1,4 +1,4 @@
-﻿package org.therapazes.luisaoproject.config;
+package org.therapazes.luisaoproject.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
