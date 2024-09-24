d6b62394812066542055a6fe84a7aa09691256a5
Dnllcs <daniellcs04@gmail.com>
2024-09-23 22:22:45 -0300

    feat: adiciona entidade e controller do produto

---
+++ src/main/java/org/therapazes/luisaoproject/entities/Produto.java
@@ -0,0 +1,28 @@
+package org.therapazes.luisaoproject.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "produto")
+public class Produto {
+    @Column(name = "idProduto")
+    private Integer idProduto;
+    @Column(name = "nome")
+    private String nome;
+    @Column(name = "codigo_listagem")
+    private Integer codigoListagem;
+    @Column(name = "descricao")
+    private String descricao;
+    @Column(name = "detalhes")
+    private String detalhes;
+    @Column(name = "categoria")
+    private String categoria;
+    @Column(name = "dataCadastro")
+    private Date dataCadastro;
+    @Column(name = "isAtivo")
+    private boolean isAtivo;
+}
