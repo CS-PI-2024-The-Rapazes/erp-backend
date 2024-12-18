b9d72e3deffbade47897521dc7f17b5315876642
Mateus Back <m.back@nextage.com.br>
2024-12-18 18:25:04 -0300

    feat: add cors config

--- src/main/java/org/therapazes/luisaoproject/config/auth/CorsConfig.java
+++ src/main/java/org/therapazes/luisaoproject/config/auth/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:3000", "https://erp-frontend-deploy.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
